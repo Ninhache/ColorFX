bbd4a6934c6b86d5e85f0454f3d5eede647b8adc
ALMEIDA Neo <neo.almeida.etu@univ-lille.fr>
2021-05-22 05:19:22 +0200

    Now App have an icon

--- src/main/java/colorfix/app/ColorFX.java
+++ src/main/java/colorfix/app/ColorFX.java
@@ -3,21 +3,27 @@ package colorfix.app;
 import colorfix.app.stages.MainStage;
 import javafx.application.Application;
 import javafx.application.HostServices;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ColorFX extends Application {
     private static HostServices services;
 
-    public static void main(String[] args) {
-        launch(args);
-    }
-
     @Override
     public void start(Stage stage) {
         services = getHostServices();
         var mainStage = new MainStage();
+
+
+        mainStage.getIcons().add(Constants.APP_ICON);
         mainStage.getScene().getStylesheets().add(getClass().getResource("/rename.css").toExternalForm());
         mainStage.show();
+
+
+    }
+
+    public static void main(String[] args) {
+        launch(args);
     }
 
     public static HostServices hostServices() {
