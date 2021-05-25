c78a3e1629f546afe7dfc3acb2efaa88a1f95419
ALMEIDA Neo <neo.almeida.etu@univ-lille.fr>
2021-05-25 22:50:42 +0200

    Calibrate Button open this stage...

---
+++ src/main/java/colorfix/app/controls/CopyTableView.java
@@ -0,0 +1,27 @@
+package colorfix.app.controls;
+
+import colorfix.app.controls.columns.*;
+import colorfix.app.enums.ColorSpace;
+import javafx.scene.control.TableView;
+import javafx.scene.paint.Color;
+
+public class CopyTableView extends TableView<Color> {
+    public CopyTableView() {
+        ColorThumbnailColumn columnThumbnail = new ColorThumbnailColumn();
+
+        //ColorComponentColumn columnRed = new ColorComponentColumn(ColorComponent.RED);
+        //ColorComponentColumn columnGreen = new ColorComponentColumn(ColorComponent.GREEN);
+        //ColorComponentColumn columnBlue = new ColorComponentColumn(ColorComponent.BLUE);
+
+        HexadecimalValueColumn columnHex = new HexadecimalValueColumn();
+
+        ColorSpaceColumn columnRGB = new ColorSpaceColumn(ColorSpace.RGB);
+        ColorSpaceColumn columnHSB = new ColorSpaceColumn(ColorSpace.HSB);
+
+        CopyActionsColumn columnAction = new CopyActionsColumn();
+
+        //getColumns().addAll(columnThumbnail, columnHex, columnRed, columnGreen, columnBlue);
+
+        getColumns().addAll(columnThumbnail, columnHex, columnRGB, columnHSB, columnAction);
+    }
+}
