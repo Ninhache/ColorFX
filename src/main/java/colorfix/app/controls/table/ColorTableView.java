c2feb72c26f1a1b0d581d8a0916f98707414db40
ALMEIDA Neo <neo.almeida.etu@univ-lille.fr>
2021-06-02 11:37:42 +0200

    Add a context menu, with working buttons, still a bit messy

--- src/main/java/colorfix/app/controls/table/ColorTableView.java
+++ src/main/java/colorfix/app/controls/table/ColorTableView.java
@@ -2,15 +2,28 @@ package colorfix.app.controls.table;
 
 import colorfix.app.controls.table.columns.*;
 import colorfix.app.enums.ColorSpace;
+import colorfix.app.stages.TestStage;
+import colorfix.app.util.ColorUtil;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.event.ActionEvent;
+import javafx.scene.control.ContextMenu;
+import javafx.scene.control.MenuItem;
+import javafx.scene.control.SeparatorMenuItem;
 import javafx.scene.control.TableView;
+import javafx.scene.input.Clipboard;
+import javafx.scene.input.ClipboardContent;
+import javafx.scene.input.MouseButton;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.paint.Color;
 
 public class ColorTableView extends TableView<Color> {
     private SimpleBooleanProperty hexVisible = new SimpleBooleanProperty(true);
     private SimpleBooleanProperty rgbVisible = new SimpleBooleanProperty(true);
-    private SimpleBooleanProperty hsbVisible = new SimpleBooleanProperty(true);
     private SimpleBooleanProperty cmykVisible = new SimpleBooleanProperty(false);
+    private SimpleBooleanProperty hsbVisible = new SimpleBooleanProperty(true);
+    ContextMenu contextMenu;
+    final Clipboard clipboard = Clipboard.getSystemClipboard();
+	final ClipboardContent content = new ClipboardContent();
 
 
     public ColorTableView() {
@@ -25,15 +38,71 @@ public class ColorTableView extends TableView<Color> {
         ColorSpaceColumn columnRGB = new ColorSpaceColumn(ColorSpace.RGB);
         ColorSpaceColumn columnHSB = new ColorSpaceColumn(ColorSpace.HSB);
         ColorSpaceColumn columnCMYK = new ColorSpaceColumn(ColorSpace.CMYK);
+        
+        setOnMouseClicked(this::onMouseClicked);
 
         columnHex.visibleProperty().bind(hexVisible);
         columnRGB.visibleProperty().bind(rgbVisible);
-        columnHSB.visibleProperty().bind(hsbVisible);
         columnCMYK.visibleProperty().bind(cmykVisible);
+        columnHSB.visibleProperty().bind(hsbVisible);
 
         ActionsColumn columnAction = new ActionsColumn();
 
         getColumns().addAll(columnThumbnail, columnHex, columnRGB, columnCMYK, columnHSB, columnAction);
+        
+        /// Menu contextuel
+        contextMenu = new ContextMenu();
+        
+        MenuItem menuItemMod = new MenuItem("Modify");
+        menuItemMod.setOnAction(this::onItemModify);
+        
+        MenuItem menuItemAdd = new MenuItem("Add");
+        menuItemAdd.setOnAction(this::onItemAdd);
+        
+        MenuItem menuItemDel = new MenuItem("Delete");
+        menuItemDel.setOnAction(this::onItemDelete);
+        
+        MenuItem menuItemCpy = new MenuItem("Copier");
+        menuItemCpy.setOnAction(this::onItemCopy);
+        
+        SeparatorMenuItem separatorMenuItem = new SeparatorMenuItem();
+        SeparatorMenuItem separatorMenuItem2 = new SeparatorMenuItem();
+        SeparatorMenuItem separatorMenuItem3 = new SeparatorMenuItem();
+        
+        contextMenu.getItems().addAll(menuItemMod,separatorMenuItem, menuItemAdd, separatorMenuItem2,menuItemDel,separatorMenuItem3,menuItemCpy);
+        
+        
+    }
+    
+    private void onMouseClicked(MouseEvent e) {
+    	if(e.getButton() == MouseButton.SECONDARY) {
+    		contextMenu.show(this, e.getScreenX(), e.getScreenY());
+    	}else {
+    		contextMenu.hide();
+    	}
+    }
+    
+    private void onItemAdd(ActionEvent e) {
+    	TestStage ok = new TestStage();
+    	ok.show();
+    	System.out.println("add");
+    }
+    
+    private void onItemModify(ActionEvent e) {
+    	int index = getFocusModel().getFocusedCell().getRow();
+    	
+    	System.out.println(getItems().get(index).toString());
+    }
+    
+    private void onItemDelete(ActionEvent e) {
+    	int index = getFocusModel().getFocusedCell().getRow();
+    	getItems().remove(index);
+    }
+    
+    private void onItemCopy(ActionEvent e) {
+    	int index = getFocusModel().getFocusedCell().getRow();
+    	content.putString(ColorUtil.tohexCode(getItems().get(index)));
+    	clipboard.setContent(content);	
     }
 
     // === PROPRIETES ===
